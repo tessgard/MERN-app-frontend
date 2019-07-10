0d806ec6c6ccd760a99f564ee6afffa2fd06a006
Tess <gard.tess@gmail.com>
2019-07-10 17:29:04 +1000

    debugging initial api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  async componentDidMount() {
+    console.log("cdm")
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/about`)
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+  }
+
+
+  render() {
+    return (
+      <>
+      <h1>BCMA</h1>
+      <h3>Dance | Music | Theatre</h3>
+      </>
+    )
+  }
 }
 
 export default App;
