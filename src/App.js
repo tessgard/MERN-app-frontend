e2a0e1e44a348ae0a23bdd0b4e14f18e33b43319
jieyiphua <jieyiphua@gmail.com>
2019-07-12 10:49:02 +1000

    changed localhost link to now

--- src/App.js
+++ src/App.js
@@ -1,62 +1,68 @@
-import React from 'react';
-import './App.css';
-import Routes from './Routes';
-import axios from 'axios';
-
+import React from "react";
+import "./App.css";
+import Routes from "./Routes";
+import axios from "axios";
 
 class App extends React.Component {
-  state = { authentication: false, errors: [], curretUser: null };
+  state = { authentication: false, errors: [], currentUser: null };
 
-  login = async (userCredentials) => {
+  login = async userCredentials => {
     console.log(userCredentials);
 
     try {
-      const response = await axios.post('http://localhost:5000/auth', userCredentials)
-      console.log(response)
-      const token = response.data.token
-      console.log(token)
-      localStorage.setItem('token', token)
-      this.setState({ authentication: true})
-      console.log(this.state)
-
+      const response = await axios.post(
+        "https://deployment-mern-backend-tessivanjayz.gardtess.now.sh/auth",
+        userCredentials
+      );
+      console.log(response);
+      const token = response.data.token;
+      console.log(token);
+      localStorage.setItem("token", token);
+      this.setState({ authentication: true });
+      console.log(this.state);
     } catch (error) {
       this.setState({
         authentication: false,
         errors: error.response.data.errors
-      })
-      console.log(error.response.data.errors)
+      });
+      console.log(error.response.data.errors);
     }
-  }
+  };
 
   logout = () => {
-    console.log('clicked')
-    localStorage.removeItem('token')
-    this.setState({ authentication: false })
-  }
+    console.log("clicked");
+    localStorage.removeItem("token");
+    this.setState({ authentication: false });
+  };
 
   componentDidMount = async () => {
     try {
-      const token = localStorage.getItem('token');
-      const authenticated = await axios.get('http://localhost:5000/auth', { headers: { 'auth-x-token': token}});
-      console.log(authenticated)
-      console.log('authenticated')
+      const token = localStorage.getItem("token");
+      const authenticated = await axios.get(
+        "https://deployment-mern-backend-tessivanjayz.gardtess.now.sh/auth",
+        {
+          headers: { "auth-x-token": token }
+        }
+      );
+      console.log(authenticated);
+      console.log("authenticated");
 
       this.setState({
         authentication: true,
-        curretUser: authenticated.data
-      })
-      console.log(this.state)
+        currentUser: authenticated.data
+      });
+      console.log(this.state);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   render() {
-    const { login, logout } = this
-    const { authentication } = this.state
+    const { login, logout } = this;
+    const { authentication } = this.state;
     return (
-       <Routes authentication={authentication} login={login} logout={logout}/> 
-    )
+      <Routes authentication={authentication} login={login} logout={logout} />
+    );
   }
 }
 
