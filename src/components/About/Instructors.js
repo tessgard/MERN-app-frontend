9511634761b3a7e8c192000e901077fd874ef41b
Tess <gard.tess@gmail.com>
2019-07-12 16:27:40 +1000

    debuggin routes

--- src/components/About/Instructors.js
+++ src/components/About/Instructors.js
@@ -6,11 +6,8 @@ class Instructors extends React.Component {
   state = {
     data: []
   };
-
   async componentDidMount() {
-    const response = await axios(
-      "https://deployment-mern-backend-tessivanjayz.gardtess.now.sh/instructor"
-    );
+    const response = await axios("http://localhost:5000/instructor");
     console.log(response);
     this.setState({
       data: response.data
@@ -20,18 +17,18 @@ class Instructors extends React.Component {
   render() {
     return (
       <div className="instructorMainContainer">
+        <h1 className="instructorHeader">Faculty</h1>
         
         <div className="instructorBottomContainer">
           <div className="instructorInnerContainer">
-            <h1 className="instructorHeader">Faculty</h1>
             <div className="instructorProfilesContainer">
               {this.state.data.map((item, index) => (
+
                 <div className="instructorProfileCard" key={index}>
-                  
                   <div className="instructorDetails">
-                    <h3>Faculty: {item.faculty}</h3>
                     <h2>Name : {item.name}</h2>
                     <h4>{item.description}</h4>
+                    <h4>Expertise : {item.expertise}</h4>
                   </div>
                 </div>
               ))}
