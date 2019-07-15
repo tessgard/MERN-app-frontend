2ae5b728da9cab8beede12e24e28a0325b86f159
Ivan Donato <donato.ivan@gmail.com>
2019-07-15 14:28:43 +1000

    added update functionality for about, accessible only on login

--- src/Routes.js
+++ src/Routes.js
@@ -29,7 +29,7 @@ import NavBar from "./components/Shared/NavBar";
 
 class Routes extends React.Component {
   render() {
-    const { authentication, login, logout } = this.props
+    const { authentication, login, logout, selectedContent, onContentSelect } = this.props
     return(
       <Switch>
         <div className="appOuterContainer">
@@ -39,7 +39,9 @@ class Routes extends React.Component {
           }}/>
           <Route exact path="/whats-on/current-productions" component={CurrentProductions} />
           <Route exact path="/whats-on/events" component={Events} />
-          <Route exact path="/about/our-story" component={OurStory} />
+          <Route exact path="/about/our-story" render={(history) => {
+            return <OurStory onContentSelect={onContentSelect} history={history} authentication={authentication}/>
+          }}/>
           <Route exact path="/about/gallery" component={ImageGallery} />
           <Route exact path="/about/instructors" component={Instructors} />
           <Route exact path="/classes/dance" component={Dance} />
@@ -53,7 +55,7 @@ class Routes extends React.Component {
             return <Login authentication={authentication} history={history} login={login} />
           }} />
           <Route exact path="/admin/about/update" render={(history) => {
-            return <UpdateAbout authentication={authentication} history={history} />
+            return <UpdateAbout authentication={authentication} history={history} selectedContent={selectedContent}/>
           }} />
           <Route exact path="/admin/class/update" render={(history) => {
             return <UpdateClass authentication={authentication} history={history} />
--- src/components/About/OurStory.js
+++ src/components/About/OurStory.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./OurStory.css";
 import axios from "axios";
+import { Link } from 'react-router-dom';
 
 class OurStory extends React.Component {
   state = {
@@ -8,7 +9,7 @@ class OurStory extends React.Component {
   };
 
   async componentDidMount() {
-    const response = await axios(
+    const response = await axios.get(
       "https://deployment-mern-backend-tessivanjayz.gardtess.now.sh/about"
     );
     console.log(response);
@@ -18,31 +19,62 @@ class OurStory extends React.Component {
   }
 
   render() {
-    console.log(this.state.data);
-    return (
-      <div className="aboutMainContainer">
+    const { onContentSelect, authentication } = this.props
+    console.log(this.props);
 
-        <div className="aboutMeDescription">
-          <h1 className="bcmaPageHeaderH1">About Us</h1>
-
-        </div>
-        <div className="aboutBottomContainer">
-          <div className="aboutBox">
-            
-            <br />
-            <br />
-            <br />
-            <br />
-            {this.state.data.map((item, index) => (
-              <div className="aboutBoxDescription" key={index}>
-                <p>{item.description}</p>
-                <br />
-              </div>
-            ))}
+    if (authentication) {
+      return (
+        <div className="aboutMainContainer">
+  
+          <div className="aboutMeDescription">
+            <h1 className="bcmaPageHeaderH1">About Us</h1>
+  
+          </div>
+          <div className="aboutBottomContainer">
+            <div className="aboutBox">
+              
+              <br />
+              <br />
+              <br />
+              <br />
+              {this.state.data.map((item, index) => (
+                <div className="aboutBoxDescription" key={index} >
+                  <p>{item.description}</p>
+                  <p>{item._id}</p>
+                  <Link to="/admin/about/update" onClick={() => onContentSelect(item)}>Update</Link>
+                  <br />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="aboutMainContainer">
+  
+          <div className="aboutMeDescription">
+            <h1 className="bcmaPageHeaderH1">About Us</h1>
+  
+          </div>
+          <div className="aboutBottomContainer">
+            <div className="aboutBox">
+              
+              <br />
+              <br />
+              <br />
+              <br />
+              {this.state.data.map((item, index) => (
+                <div className="aboutBoxDescription" key={index} >
+                  <p>{item.description}</p>
+                  <br />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
