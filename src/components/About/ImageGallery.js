a8ec994c5c365b6a170a679b0bce18551cae2c20
Tess <gard.tess@gmail.com>
2019-07-17 14:17:33 +1000

    image gallery popup done

--- src/components/About/ImageGallery.js
+++ src/components/About/ImageGallery.js
@@ -4,21 +4,40 @@ import axios from "axios";
 
 class ImageGallery extends React.Component {
   state = {
-    images: null
+    images: null,
+    popup: false,
+    currentImage: null
+
   };
 
   async componentDidMount() {
     const response = await axios.get(
       "https://deployment-mern-backend-tessivanjayz.gardtess.now.sh/images/get-objects"
     );
-    console.log(response);
+    // console.log(response);
     this.setState({
       images: response.data
     });
 
-    
   }
 
+  imagePopup = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget.src)
+
+    this.setState({
+      popup: true,
+      currentImage: e.currentTarget.src
+
+
+    })  
+  };  
+
+  removePopup = () => {
+    this.setState({
+      popup: false,
+    })
+  }
   
 
   render() {
@@ -27,26 +46,31 @@ class ImageGallery extends React.Component {
       return null
     } else {
       const imageArr = imageState.imageUrls
-      console.log(imageArr)
       return (
         
         <div className="main-container">
+              <div id="overlay" className={this.state.popup && "show" }></div>
+
           <div className="inner-main-container">
           <h1 className="bcmaPageHeaderH1">Image Gallery</h1>
             <div className="gallery-outer-container">
-            
-            {imageArr.slice(1).map((url ) => (
-              <div className="image-thumbnail">   
-                <img className="images" src={url}></img>
-              </div>
+            {imageArr.slice(1).map((url) => (
+                 <img onClick={this.imagePopup}className="images" src={url}></img>
           ))}
-
-
-
-      
-
-
             </div>
+
+          {
+            this.state.popup &&
+              <div className="image-show-popup">
+                <div className="image-show-image-card">
+                  
+                  <img className="image-popup-large" src={this.state.currentImage}></img>
+                  <br></br>
+                  <a id="closing-link-image" href="#"><h2 onClick={this.removePopup}>x</h2></a>
+                </div>
+              </div>
+          }
+
           </div>
         </div>
         
